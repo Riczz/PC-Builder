c9956983745222bb492c3dded7282ed863742c4f
Riczz <csinosricsi@gmail.com>
2022-04-10 18:08:59 +0200

    Save build
    Bugfix: CreateBuildActivity restarts on screen rotation
    Bugfix: Hardwares have null reference on HardwareType
    Added ViewModel class for BuildsListFragment
    Added build save functionality
    Refactored code

---
+++ app/src/main/java/com/riczz/pcbuilder/BuildViewModel.java
@@ -0,0 +1,22 @@
+package com.riczz.pcbuilder;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.riczz.pcbuilder.model.BuildItem;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class BuildViewModel extends ViewModel {
+
+    private final MutableLiveData<ArrayList<BuildItem>> builds = new MutableLiveData<>(new ArrayList<>());
+
+    public void addBuild(BuildItem item) {
+        Objects.requireNonNull(builds.getValue()).add(item);
+    }
+
+    public ArrayList<BuildItem> getBuilds() {
+        return builds.getValue();
+    }
+}
